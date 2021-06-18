d5378471d23c16f1914690cc98d5acaa572193a0
diegoesp28 <81206986+diegoesp28@users.noreply.github.com>
2021-06-18 00:46:35 -0400

    Delete core directory

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import animal, mascotas
-
-# Register your models here.
-#permite administrar el modelo completo
-
-
-admin.site.register(animal)
-admin.site.register(mascotas)
